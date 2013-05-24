c8f2ea222da8d06b2c108582556efca8af993392
Harel M <Harel.Mazor@gmail.com>
2013-05-24 16:28:29 +0300

    Forest perimiter Added but i could not make it work correct...

--- IsraelHiking/AddWayLengthTags.py
+++ IsraelHiking/AddWayLengthTags.py
@@ -7,25 +7,48 @@ import math
 # Create a sorted list of main roads / nodes
 # osmWayList = OsmData.load_pbf_file("D:\Tiles\OSM\israel_and_palestine.osm.pbf")
 
+#this function will calculate the distance between two nodes
+def getLength(node1, node2):
+	startx = node1.location.x
+	starty = node1.location.y
+	endx = node2.location.x
+	endy = node2.location.y
+	midx = (startx+endx)/2
+	midy = (starty+endy)/2
+	# Earth's circumference is about 40,000 km.
+	# So 1 degree of longitude at the equator, or 1 degree of latitude, is about 40,000/360 = 110 km.
+	distx = 40000*(endx-startx)/360 * math.cos(math.radians(midy))
+	disty = 40000*(endy-starty)/360
+	length = math.sqrt(distx*distx+disty*disty)
+	return length
+
 # Look for the first OSM map source.
 for layer in Map.layers:
     if layer.layer_type == "OsmLayer":
         osmWayList = layer.osm
 
+	#adding length to highways
 	for way in osmWayList.find_ways(lambda x : x.has_tag("highway")):
 	    if way.nodes[0]!=way.nodes[way.nodes_count-1]:
-		startx = osmWayList.node(way.nodes[0]).location.x
-		starty = osmWayList.node(way.nodes[0]).location.y
-		endx = osmWayList.node(way.nodes[way.nodes_count-1]).location.x
-		endy = osmWayList.node(way.nodes[way.nodes_count-1]).location.y
-		midx = (startx+endx)/2
-		midy = (starty+endy)/2
-		# Earth's circumference is about 40,000 km.
-		# So 1 degree of longitude at the equator, or 1 degree of latitude, is about 40,000/360 = 110 km.
-		distx = 40000*(endx-startx)/360 * math.cos(math.radians(midy))
-		disty = 40000*(endy-starty)/360
-		length = math.sqrt(distx*distx+disty*disty)
-		osmWayList.way(way.id).set_tag("length", str(length))
+			length = getLength(osmWayList.node(way.nodes[0]), osmWayList.node(way.nodes[way.nodes_count-1]))
+			osmWayList.way(way.id).set_tag("length", str(length))
+	
+	#adding perimeter to forests area
+	for way in osmWayList.find_ways(lambda x : (x.has_tag("landuse") or x.has_tag("natural")) ):
+	    #if way.nodes[0]==way.nodes[way.nodes_count-1] : 
+		osmWay = osmWayList.way(way.id)
+		if osmWay.has_tag("landuse") and osmWay.get_tag("landuse")=="forest" :
+			perimeter = 0
+			for i in range(way.nodes_count-1) :
+				length = getLength(osmWayList.node(way.nodes[i]), osmWayList.node(way.nodes[i+1]))
+				perimeter = perimeter + length
+			osmWay.set_tag("length", str(perimeter))
+		if osmWay.has_tag("natural") and osmWay.get_tag("natural")=="wood" :
+			perimeter = 0
+			for i in range(way.nodes_count-1) :
+				length = getLength(osmWayList.node(way.nodes[i]), osmWayList.node(way.nodes[i+1]))
+				perimeter = perimeter + length
+			osmWay.set_tag("length", str(perimeter))
 
 # If there are no OSM map sources, report an error...
 if osmWayList == None:
