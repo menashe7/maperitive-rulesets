9168858e92117e0c146734970011a10b9bcf653f
Zeev Stadler <ze.ev.stadler@gmail.com>
2013-05-31 11:55:02 +0300

    Relative program paths in tile uploading script

--- IsraelHiking/UploadTiles.bat
+++ IsraelHiking/UploadTiles.bat
@@ -3,7 +3,7 @@ SET ZIPFILE="%~nx1"
 IF %ZIPFILE%=="" SET ZIPFILE=TileUpdate.zip
 %~d0
 cd %~dp0
-"C:\Program Files (x86)\WinSCP\WinSCP.com" /command "open ""Upload-osm.org.il""" "option reconnect 15" "option batch abort" "put -resume -preservetime -transfer=binary ""%ZIPFILE%""" "exit" 
+"..\..\WinSCP\WinSCP.com" /command "open ""Upload-osm.org.il""" "option reconnect 15" "option batch abort" "put -resume -preservetime -transfer=binary ""%ZIPFILE%""" "exit" 
 REM unzip
-"C:\Program Files (x86)\PuTTY\plink.exe" osm.org.il unzip -d ~/public_html/Israel\ Hiking/Tiles -o ~/temp/%ZIPFILE%
-"C:\Program Files (x86)\PuTTY\plink.exe" osm.org.il rm ~/temp/%ZIPFILE%
+"..\..\PuTTY\plink.exe" osm.org.il unzip -d ~/public_html/Israel\ Hiking/Tiles -o ~/temp/%ZIPFILE%
+"..\..\PuTTY\plink.exe" osm.org.il rm ~/temp/%ZIPFILE%
